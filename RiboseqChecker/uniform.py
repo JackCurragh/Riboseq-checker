d17149623aecad0379aa8294d092f207195cbf92
Jack Tierney <jack.tierney@student.ucc.ie>
2023-02-27 19:52:50 +0000

    updated uniformity checker

--- RiboseqChecker/uniform.py
+++ RiboseqChecker/uniform.py
@@ -6,14 +6,29 @@ import argparse
 import pandas as pd
 from scipy.stats import kstest, uniform
 import numpy as np
+import math
 
 def main(args):
     """
     wrapper function to obtain the uniformity score
     """
-    rng = np.random.default_rng()
-    print(kstest(uniform.rvs(size=5, random_state=rng), [1,2,3,4,5]))
+    # Read in the bed file
+    df = pd.read_csv(args.bed, sep='\t', header=None)
+    print(df)
+    # Get the read counts
+    for transcript in df[0].unique():
+        print(transcript)
+        # Get the read counts for this transcript
+        df_transcript = df[df[0] == transcript]
+        # Get the read counts
+        counts = np.array(df_transcript[3].values)
+        # calculate total number of reads
+        total_reads = sum(counts)
+        # calculate probability distribution of coverage values
+        probability_distribution = [float(count) / total_reads for count in counts]
 
+        max_entropy = -sum(p * math.log(p, 2) for p in probability_distribution if p != 0)
+        print(max_entropy)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Obtain a score for the uniformity of a ribosome profile')
